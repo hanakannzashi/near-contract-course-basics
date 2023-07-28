9ef00396ff773ff31f56a465f4cc51a56d689033
sakura <zjchen1234@foxmail.com>
2023-07-28 13:13:39 +0800

    第四章

--- 4. 跨合约调用与回调函数/src/lib.rs
+++ 4. 跨合约调用与回调函数/src/lib.rs
@@ -1,23 +1,28 @@
 mod cross;
 
+use crate::cross::linkdrop_contract;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, near_bindgen, AccountId, PublicKey, Promise, log, PromiseError, PromiseOrValue, PanicOnDefault, Gas, GasWeight, serde_json};
 use near_sdk::json_types::U128;
 use near_sdk::serde::Serialize;
-use crate::cross::linkdrop_contract;
+use near_sdk::{
+    env, log, near_bindgen, serde_json, AccountId, Gas, GasWeight, PanicOnDefault, Promise,
+    PromiseError, PromiseOrValue, PublicKey,
+};
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
     // linkdrop 合约地址
-    linkdrop_contract_id: AccountId
+    linkdrop_contract_id: AccountId,
 }
 
 #[near_bindgen]
 impl Contract {
     #[init]
     pub fn init(linkdrop_contract_id: AccountId) -> Self {
-        Self { linkdrop_contract_id }
+        Self {
+            linkdrop_contract_id,
+        }
     }
 
     // 通过 linkdrop 合约创建一个子账户，需要支付一定的 NEAR 作为初始余额，如果创建失败，则退回这笔费用
@@ -26,7 +31,7 @@ impl Contract {
     pub fn create_account_by_linkdrop(
         &mut self,
         new_account_id: AccountId,
-        new_public_key: PublicKey
+        new_public_key: PublicKey,
     ) -> PromiseOrValue<()> {
         let amount = env::attached_deposit();
         // `U128` 是 `u128` 的封装类型，使用 `String` 的 json 序列化方式
@@ -47,7 +52,7 @@ impl Contract {
     pub fn create_account_by_linkdrop_use_promise_directly(
         &mut self,
         new_account_id: AccountId,
-        new_public_key: PublicKey
+        new_public_key: PublicKey,
     ) -> PromiseOrValue<()> {
         let amount = env::attached_deposit();
         let wrapped_amount = U128(amount);
@@ -56,14 +61,14 @@ impl Contract {
         #[serde(crate = "near_sdk::serde")]
         struct CreateAccountArgs {
             new_account_id: AccountId,
-            new_public_key: PublicKey
+            new_public_key: PublicKey,
         }
 
         #[derive(Serialize)]
         #[serde(crate = "near_sdk::serde")]
         struct ResolveCreateAccountArgs {
             payer_id: AccountId,
-            amount: U128
+            amount: U128,
         }
 
         Promise::new(self.linkdrop_contract_id.clone())
@@ -71,31 +76,34 @@ impl Contract {
                 "create_account".to_string(),
                 serde_json::to_vec(&CreateAccountArgs {
                     new_account_id,
-                    new_public_key
-                }).unwrap(),
-                amount,             // 附带 NEAR 用于创建账户
-                Gas(0),        // 不分配固定的 gas
-                GasWeight(1) // 剩余 gas 分配时占 1 份
-            ).then(
-                Promise::new(env::current_account_id())
-                    .function_call_weight(
-                        "resolve_create_account".to_string(),
-                        serde_json::to_vec(&ResolveCreateAccountArgs {
-                            payer_id: env::predecessor_account_id(),
-                            amount: wrapped_amount
-                        }).unwrap(),
-                        0,
-                        Gas(0),        // 不分配固定的 gas
-                        GasWeight(1) // 剩余 gas 分配时占 1 份
-                    )
-            ).into()
+                    new_public_key,
+                })
+                .unwrap(),
+                amount,       // 附带 NEAR 用于创建账户
+                Gas(0),       // 不分配固定的 gas
+                GasWeight(1), // 剩余 gas 分配时占 1 份
+            )
+            .then(
+                Promise::new(env::current_account_id()).function_call_weight(
+                    "resolve_create_account".to_string(),
+                    serde_json::to_vec(&ResolveCreateAccountArgs {
+                        payer_id: env::predecessor_account_id(),
+                        amount: wrapped_amount,
+                    })
+                    .unwrap(),
+                    0,
+                    Gas(0),       // 不分配固定的 gas
+                    GasWeight(1), // 剩余 gas 分配时占 1 份
+                ),
+            )
+            .into()
     }
 
     pub fn resolve_create_account(
         &mut self,
         payer_id: AccountId,
         amount: U128,
-        #[callback_result] is_success: Result<bool, PromiseError>
+        #[callback_result] is_success: Result<bool, PromiseError>,
     ) {
         if is_success.unwrap_or(false) {
             log!("Account is successfully created.");
