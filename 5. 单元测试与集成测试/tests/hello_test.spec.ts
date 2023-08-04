ffa5a65751a6632ad429c7ee48294b94e61b4c33
sakura <zjchen1234@foxmail.com>
2023-08-04 18:15:28 +0800

    第五章

--- 5. 单元测试与集成测试/tests/hello_test.spec.ts
+++ 5. 单元测试与集成测试/tests/hello_test.spec.ts
@@ -1,40 +1,40 @@
-import {initContext, Context} from "./utils";
+import { initContext, Context } from './utils';
 
 let context: Context;
 
 beforeAll(async () => {
   // 初始化全局上下文
-  context = await initContext()
-})
+  context = await initContext();
+});
 
 afterAll(async () => {
-  const { worker } = context
+  const { worker } = context;
   // 记得关 sandbox, 不然多跑几次内存就满了
   await worker.tearDown();
-})
+});
 
 test('Test setter without permission', async () => {
-  const { contract, root } = context
+  const { contract, root } = context;
 
   const promise = root.call<void>(contract, 'set_account_description', {
     account_id: 'bob.near',
-    description: 'Nice Bob'
-  })
+    description: 'Nice Bob',
+  });
 
-  await expect(promise).rejects.toThrow('Only contract owner can call this method.')
-})
+  await expect(promise).rejects.toThrow('Only contract owner can call this method.');
+});
 
 test('Test setter getter', async () => {
-  const { contract, alice } = context
+  const { contract, alice } = context;
 
   await alice.call(contract, 'set_account_description', {
     account_id: 'bob.near',
-    description: 'Nice Bob'
-  })
+    description: 'Nice Bob',
+  });
 
   const description = await contract.view<string>('get_account_description', {
     account_id: 'bob.near',
-  })
+  });
 
-  expect(description).toEqual('Nice Bob')
-})
+  expect(description).toEqual('Nice Bob');
+});
--- 5. 单元测试与集成测试/tests/utils.ts
+++ 5. 单元测试与集成测试/tests/utils.ts
@@ -1,40 +1,41 @@
-import fs from "fs";
-import {NearAccount, Worker} from "near-workspaces";
+import fs from 'fs';
+import { NearAccount, Worker } from 'near-workspaces';
 
 export interface Context {
-  worker: Worker,
-  root: NearAccount,
-  contract: NearAccount,
-  alice: NearAccount
+  worker: Worker;
+  root: NearAccount;
+  contract: NearAccount;
+  alice: NearAccount;
 }
 
 export async function initContext(): Promise<Context> {
   const worker = await Worker.init({
     network: 'sandbox',
-    rm: true // 关闭 sandbox 后删除测试缓存文件
-  })
+    rm: true, // 关闭 sandbox 后删除测试缓存文件, 节约硬盘空间
+  });
 
   // 默认根账户名为 `test.near`
-  const root = worker.rootAccount
+  const root = worker.rootAccount;
 
   // `hello_test.test.near`
-  const contract = await root.createSubAccount('hello_test')
+  const contract = await root.createSubAccount('hello_test');
 
   // `alice.test.near`
-  const alice = await root.createSubAccount('alice')
+  const alice = await root.createSubAccount('alice');
 
-  const code = fs.readFileSync('res/hello_test.wasm')
+  const code = fs.readFileSync('res/hello_test.wasm');
 
   // 部署并初始化合约
-  await contract.batch(contract.accountId)
+  await contract
+    .batch(contract.accountId)
     // Deploy Action
     .deployContract(code)
     // FunctionCall Action
     .functionCall('init', {
-      owner_id: alice.accountId
+      owner_id: alice.accountId,
     })
     // 执行交易
-    .transact()
+    .transact();
 
-  return { worker, root, contract, alice }
+  return { worker, root, contract, alice };
 }
